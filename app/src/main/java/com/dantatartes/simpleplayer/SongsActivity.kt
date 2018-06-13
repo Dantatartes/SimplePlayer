7ab6935541270c09195935a0717ee804e82fd991
Danis <danis.ds@yandex.ru>
2018-06-13 08:51:24 +0300

    Initial commit

---
+++ app/src/main/java/com/dantatartes/simpleplayer/SongsActivity.kt
@@ -0,0 +1,12 @@
+package com.dantatartes.simpleplayer
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SongsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_songs)
+    }
+}
