9b26e5adca60c5b9a4be6eea4675605e4cae4dcc
Danis <danis.ds@yandex.ru>
2018-06-22 14:19:41 +0300

    Add 'androidRuntimePermission' method

--- app/src/main/java/com/dantatartes/simpleplayer/MainActivity.kt
+++ app/src/main/java/com/dantatartes/simpleplayer/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.dantatartes.simpleplayer
 
+import android.Manifest
+import android.app.AlertDialog
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
 import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
@@ -11,9 +16,12 @@ import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        androidRuntimePermission()
 
         val viewPager = findViewById<ViewPager>(R.id.viewPager)
         val actionBar = supportActionBar
@@ -38,6 +46,50 @@ class MainActivity : AppCompatActivity() {
         }
 
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun androidRuntimePermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                if (shouldShowRequestPermissionRationale(Manifest.permission.READ_EXTERNAL_STORAGE)) {
+
+                    val alertBuilder = AlertDialog.Builder(this@MainActivity)
+                    alertBuilder.setMessage("External Storage Permission is Required.")
+                    alertBuilder.setTitle("Please Grant Permission.")
+                    alertBuilder.setPositiveButton("OK") { _, _ ->
+                        ActivityCompat.requestPermissions(
+                                this@MainActivity,
+                                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                                RUNTIME_PERMISSION_CODE
+                        )
+                    }
+
+                    alertBuilder.setNeutralButton("Cancel", null)
+                    val dialog = alertBuilder.create()
+                    dialog.show()
+
+                } else {
+                    ActivityCompat.requestPermissions(
+                            this@MainActivity,
+                            arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                            RUNTIME_PERMISSION_CODE
+                    )
+                }
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            RUNTIME_PERMISSION_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                }
+            }
+        }
+    }
+
+    companion object {
+        const val RUNTIME_PERMISSION_CODE = 7
     }
 }
 
